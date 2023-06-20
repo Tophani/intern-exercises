02f57646c84b2bf0320a73ee5b04ca63132f2029
Theresa <ekwonutessy@gmail.com>
2023-06-20 16:57:08 +0100

    initial commit of the git test project

---
+++ intern.js
@@ -0,0 +1,41 @@
+
+
+
+// let ageInString = 'age';
+// const chidub = {
+//     firstName: 'chidubem',
+//     lastName: 'Ulasi',
+//     age: 20,
+//     color: 'red',
+//     movies: ['Shadow hunters', 'dickinson'],
+//     music: {
+//         genres: ['rnb', 'hiphop'],
+//         songs: ['fallback', 'tripping'],
+//         artist: ['Brent', 'sam smith', 'savage']
+//     },
+//     getFullName(){
+//         return `${this.firstName} ${this.lastName}`;
+//     }
+// };
+// chidub.color = 'red';
+// chidub['bestie'] = 'samuel';
+
+
+
+// let favoriteColor = chidub.color;
+// console.log(`favorite color is ${favoriteColor}`);
+
+// let name = chidub['firstName']
+// let age = chidub[`${ageInString}`];
+
+// // for in try out
+// for (const chidubKey in chidub) {
+//     console.log(`The value of ${chidubKey} IS ${chidub[chidubKey]}`);
+// }
+
+// console.log(chidub.getFullName());
+
+
+document.getElementById();
+
+// document
